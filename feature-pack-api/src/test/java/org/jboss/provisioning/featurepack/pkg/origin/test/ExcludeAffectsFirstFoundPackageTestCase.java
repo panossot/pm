900eb6918292ea8590f1beae7da6e3cb1345cd84
Alexey Loubyansky <loubyansky@gmail.com>
2018-02-22 17:29:47 +0100

    making the origin element optional for the packages

---
+++ feature-pack-api/src/test/java/org/jboss/provisioning/featurepack/pkg/origin/test/ExcludeAffectsFirstFoundPackageTestCase.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016-2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.provisioning.featurepack.pkg.origin.test;
+
+import org.jboss.provisioning.ArtifactCoords;
+import org.jboss.provisioning.ArtifactCoords.Gav;
+import org.jboss.provisioning.ProvisioningDescriptionException;
+import org.jboss.provisioning.ProvisioningException;
+import org.jboss.provisioning.config.FeaturePackConfig;
+import org.jboss.provisioning.config.ProvisioningConfig;
+import org.jboss.provisioning.repomanager.FeaturePackRepositoryManager;
+import org.jboss.provisioning.state.ProvisionedFeaturePack;
+import org.jboss.provisioning.state.ProvisionedState;
+import org.jboss.provisioning.test.PmProvisionConfigTestBase;
+import org.jboss.provisioning.test.util.fs.state.DirState;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class ExcludeAffectsFirstFoundPackageTestCase extends PmProvisionConfigTestBase {
+
+    private static final Gav FP1_GAV = ArtifactCoords.newGav("org.pm.test", "fp1", "1.0.0.Final");
+    private static final Gav FP2_GAV = ArtifactCoords.newGav("org.pm.test", "fp2", "1.0.0.Final");
+    private static final Gav FP3_GAV = ArtifactCoords.newGav("org.pm.test", "fp3", "1.0.0.Final");
+
+    @Override
+    protected void setupRepo(FeaturePackRepositoryManager repoManager) throws ProvisioningDescriptionException {
+        repoManager.installer()
+        .newFeaturePack(FP1_GAV)
+            .addDependency(FeaturePackConfig.builder(FP2_GAV).build())
+            .newPackage("p1", true)
+                .writeContent("fp1/p1.txt", "p1")
+                .getFeaturePack()
+            .getInstaller()
+        .newFeaturePack(FP2_GAV)
+            .addDependency(FeaturePackConfig.builder(FP3_GAV).build())
+            .newPackage("p1", true)
+                .writeContent("fp2/p1.txt", "p1")
+                .getFeaturePack()
+            .getInstaller()
+        .newFeaturePack(FP3_GAV)
+             .newPackage("p1", true)
+                 .writeContent("fp3/p1.txt", "p1")
+                 .getFeaturePack()
+            .getInstaller()
+        .install();
+    }
+
+    @Override
+    protected ProvisioningConfig provisioningConfig() throws ProvisioningException {
+        return ProvisioningConfig.builder()
+                .addFeaturePackDep(FeaturePackConfig.builder(FP1_GAV)
+                        .excludePackage("p1")
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected ProvisionedState provisionedState() throws ProvisioningException {
+        return ProvisionedState.builder()
+                .addFeaturePack(ProvisionedFeaturePack.builder(FP3_GAV)
+                        .addPackage("p1")
+                        .build())
+                .addFeaturePack(ProvisionedFeaturePack.builder(FP2_GAV)
+                        .addPackage("p1")
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected DirState provisionedHomeDir() {
+        return newDirBuilder()
+                .addFile("fp2/p1.txt", "p1")
+                .addFile("fp3/p1.txt", "p1")
+                .build();
+    }
+}
---
+++ feature-pack-api/src/test/java/org/jboss/provisioning/featurepack/pkg/origin/test/ResolveFirstPackageOnFpDepBranchTestCase.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2016-2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.provisioning.featurepack.pkg.origin.test;
+
+import org.jboss.provisioning.ArtifactCoords;
+import org.jboss.provisioning.ArtifactCoords.Gav;
+import org.jboss.provisioning.ProvisioningDescriptionException;
+import org.jboss.provisioning.ProvisioningException;
+import org.jboss.provisioning.config.FeaturePackConfig;
+import org.jboss.provisioning.config.ProvisioningConfig;
+import org.jboss.provisioning.repomanager.FeaturePackRepositoryManager;
+import org.jboss.provisioning.state.ProvisionedFeaturePack;
+import org.jboss.provisioning.state.ProvisionedState;
+import org.jboss.provisioning.test.PmProvisionConfigTestBase;
+import org.jboss.provisioning.test.util.fs.state.DirState;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class ResolveFirstPackageOnFpDepBranchTestCase extends PmProvisionConfigTestBase {
+
+    private static final Gav FP1_GAV = ArtifactCoords.newGav("org.pm.test", "fp1", "1.0.0.Final");
+    private static final Gav FP2_GAV = ArtifactCoords.newGav("org.pm.test", "fp2", "1.0.0.Final");
+    private static final Gav FP3_GAV = ArtifactCoords.newGav("org.pm.test", "fp3", "1.0.0.Final");
+    private static final Gav FP4_GAV = ArtifactCoords.newGav("org.pm.test", "fp4", "1.0.0.Final");
+    private static final Gav FP5_GAV = ArtifactCoords.newGav("org.pm.test", "fp5", "1.0.0.Final");
+
+    @Override
+    protected void setupRepo(FeaturePackRepositoryManager repoManager) throws ProvisioningDescriptionException {
+        repoManager.installer()
+        .newFeaturePack(FP1_GAV)
+            .addDependency(FeaturePackConfig.builder(FP2_GAV).setInheritPackages(false).build())
+            .addDependency(FeaturePackConfig.builder(FP3_GAV).setInheritPackages(false).build())
+            .addDependency(FeaturePackConfig.builder(FP5_GAV).setInheritPackages(false).build())
+            .newPackage("p1", true)
+                .addDependency("p2")
+                .writeContent("fp1/p1.txt", "p1")
+                .getFeaturePack()
+            .getInstaller()
+        .newFeaturePack(FP2_GAV)
+            .newPackage("p1", true)
+                .writeContent("fp2/p1.txt", "p1")
+                .getFeaturePack()
+            .getInstaller()
+        .newFeaturePack(FP3_GAV)
+            .addDependency(FeaturePackConfig.builder(FP4_GAV).setInheritPackages(false).build())
+             .newPackage("p1", true)
+                 .writeContent("fp3/p1.txt", "p1")
+                 .getFeaturePack()
+            .getInstaller()
+        .newFeaturePack(FP4_GAV)
+            .newPackage("p1", true)
+                .writeContent("fp4/p1.txt", "p1")
+                .getFeaturePack()
+            .newPackage("p2")
+                .writeContent("fp4/p2.txt", "p2")
+                .getFeaturePack()
+           .getInstaller()
+       .newFeaturePack(FP5_GAV)
+           .newPackage("p1", true)
+               .writeContent("fp5/p1.txt", "p1")
+               .getFeaturePack()
+           .newPackage("p2")
+               .writeContent("fp5/p2.txt", "p2")
+               .getFeaturePack()
+          .getInstaller()
+        .install();
+    }
+
+    @Override
+    protected ProvisioningConfig provisioningConfig() throws ProvisioningException {
+        return ProvisioningConfig.builder()
+                .addFeaturePackDep(FP1_GAV)
+                .build();
+    }
+
+    @Override
+    protected ProvisionedState provisionedState() throws ProvisioningException {
+        return ProvisionedState.builder()
+                .addFeaturePack(ProvisionedFeaturePack.builder(FP4_GAV)
+                        .addPackage("p2")
+                        .build())
+                .addFeaturePack(ProvisionedFeaturePack.builder(FP1_GAV)
+                        .addPackage("p1")
+                        .build())
+                .build();
+    }
+
+    @Override
+    protected DirState provisionedHomeDir() {
+        return newDirBuilder()
+                .addFile("fp1/p1.txt", "p1")
+                .addFile("fp4/p2.txt", "p2")
+                .build();
+    }
+}
